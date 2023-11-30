0eb07958fa24bbcb5709c7bdb48857276821723c
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2023-11-30 12:32:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,16 +25,14 @@ int main() {
     std::cout << "Ingrese el nombre del segundo portero: ";
     std::getline(std::cin, nombrePortero2);
 
-    // Crear jugadores y porteros
-    Jugador* jugador1 = new Jugador(nombreJugador1, 90);
-    Portero* portero1 = new Portero(nombrePortero1, 80);
-    Jugador* jugador2 = new Jugador(nombreJugador2, 85);
-    Portero* portero2 = new Portero(nombrePortero2, 75);
-
     // Crear partidos
-    Partido* partido1 = new Partido(jugador1, portero1);
-    Partido* partido2 = new Partido(jugador2, portero2);
+    Partido* partido1 = new Partido();
+    Partido* partido2 = new Partido();
 
+    // Actualizar los partidos con los nombres de jugadores y porteros
+    partido1->actualizarPartido(nombreJugador1, nombrePortero1);
+    partido2->actualizarPartido(nombreJugador2, nombrePortero2);
+  
     // Ejecutar penales y contar goles
     for (int i = 0; i < 5; ++i) {
         partido1->ejecutarPenal();
@@ -50,23 +48,19 @@ int main() {
 
     // Calcular y mostrar resultado final
     std::cout << "\nResultado Final:\n";
-    std::cout << jugador1->getNombre() << ": " << golesJugador1 << " goles\n";
-    std::cout << jugador2->getNombre() << ": " << golesJugador2 << " goles\n";
+    std::cout << nombreJugador1 << ": " << golesJugador1 << " goles\n";
+    std::cout << nombreJugador2 << ": " << golesJugador2 << " goles\n";
 
     // Determinar y mostrar al ganador
     if (golesJugador1 > golesJugador2) {
-        std::cout << jugador1->getNombre() << " es el ganador.\n";
+        std::cout << nombreJugador1 << " es el ganador.\n";
     } else if (golesJugador1 < golesJugador2) {
-        std::cout << jugador2->getNombre() << " es el ganador.\n";
+        std::cout << nombreJugador2 << " es el ganador.\n";
     } else {
         std::cout << "¡Empate!\n";
     }
 
     // Liberar memoria
-    delete jugador1;
-    delete portero1;
-    delete jugador2;
-    delete portero2;
     delete partido1;
     delete partido2;
 
