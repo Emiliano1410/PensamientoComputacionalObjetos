20fcbfcbe812b73b1b5688953828c0c24f6cb173
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2023-11-30 12:31:23 -0600

    Update Partido.h

--- Partido.h
+++ Partido.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Participante.h"
 #include "Portero.h"
+#include "Jugador.h"
 #include <iostream>
 
 // Clase que representa un partido de fútbol con un equipo local, un portero visitante y un marcador de goles.
@@ -14,23 +15,32 @@ public:
     // Constructor por defecto que inicializa los punteros a nullptr y el contador de goles a cero.
     Partido();
     // Constructor que toma un participante como equipo local, un portero visitante y inicializa el contador de goles a cero.
-    Partido(Participante* equipoLocal, Portero* porteroVisitante);
+    Partido(const std::string& nombreJugador, const std::string& nombrePortero);
+    // Destructor
+    ~Partido();
     // Método para ejecutar un penal, solicitando al usuario el lado del tiro y determinando si se anota un gol o es atajado.
     void ejecutarPenal();
     // Método para obtener el número de goles anotados por el equipo local.
     int getGolesEquipoLocal() const;
+    // Método para actualizar los nombres de los jugadores y porteros
+    void actualizarPartido(const std::string& nombreJugador, const std::string& nombrePortero);
 };
 
 // Implementación del constructor por defecto.
 Partido::Partido() : equipoLocal(nullptr), porteroVisitante(nullptr), golesEquipoLocal(0) {}
-// Implementación del constructor que toma un participante como equipo local y un portero visitante.
-Partido::Partido(Participante* equipoLocal, Portero* porteroVisitante)
-    : equipoLocal(equipoLocal), porteroVisitante(porteroVisitante), golesEquipoLocal(0) {}
+// Implementación del constructor que toma nombres de jugador y portero como parámetros
+Partido::Partido(const std::string& nombreJugador, const std::string& nombrePortero)
+    : equipoLocal(new Jugador(nombreJugador, 90)), porteroVisitante(new Portero(nombrePortero, 80)), golesEquipoLocal(0) {}
 
+// Implementación del destructor
+Partido::~Partido() {
+    delete equipoLocal;
+    delete porteroVisitante;
+}
 // Implementación del método para ejecutar un penal.
 void Partido::ejecutarPenal() {
     std::string lado;
-    
+
     // Ciclo do-while para garantizar una entrada válida del usuario.
     do {
         std::cout << "Turno de " << equipoLocal->getNombre() << ". ¿A qué lado quieres tirar? (izquierda/derecha/centro): ";
@@ -58,4 +68,14 @@ void Partido::ejecutarPenal() {
 // Implementación del método para obtener el número de goles del equipo local.
 int Partido::getGolesEquipoLocal() const {
     return golesEquipoLocal;
+}
+
+// Implementacion del metodo para actualizar los nombres de los jugadores y porteros
+void Partido::actualizarPartido(const std::string& nombreJugador, const std::string& nombrePortero) {
+    delete equipoLocal;  // Liberar memoria del jugador existente
+    delete porteroVisitante;  // Liberar memoria del portero existente
+
+    // Crear nuevos jugadores y porteros con los nombres proporcionados
+    equipoLocal = new Jugador(nombreJugador, 90);
+    porteroVisitante = new Portero(nombrePortero, 80);
 }
